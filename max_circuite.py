245116f37d800ad5da4361e8bd5475606596f8e2
zopher3 <146519503+zopher3@users.noreply.github.com>
2023-10-23 17:35:44 +0100

    Update max_circuite.py

--- max_circuite.py
+++ max_circuite.py
@@ -1,17 +1,27 @@
 import stem.process
+from stem import Signal
+def circuite_max():
+    # Caminho para o arquivo de configuração do Tor
+    torrc_path = "/etc/tor/torrc"  # Altere o caminho conforme necessário
 
-# Caminho para o arquivo de configuração do Tor
-torrc_path = "/etc/tor/torrc"  # Altere o caminho conforme necessário
+    # Defina a nova configuração MaxCircuitDirtiness (10 minutos)
+    new_max_circuit_dirtiness = "MaxCircuitDirtiness 10 minutes"
 
-# Defina a nova configuração MaxCircuitDirtiness (10 segundos)
-new_max_circuit_dirtiness = "MaxCircuitDirtiness 10 seconds"
+    try:
+        # Inicialize o processo Tor com o arquivo de configuração
+        tor_process = stem.process.launch_tor_with_config(
+            torrc_path=torrc_path,
+        )
 
-try:
-    # Inicialize o processo Tor com o arquivo de configuração
-    tor_process = stem.process.launch_tor_with_config(
-        torrc_path=torrc_path,
-    )
+        # Crie um controlador para interagir com o Tor
+        with tor_process.controller as controller:
+            # Defina a nova configuração MaxCircuitDirtiness
+            controller.set_conf("MaxCircuitDirtiness", new_max_circuit_dirtiness)
 
-    print(f"A configuração MaxCircuitDirtiness foi definida para {new_max_circuit_dirtiness}.")
-except Exception as e:
-    print(f"Erro: {e}")
+            # Envie um sinal para recarregar a configuração
+            controller.signal(Signal.HUP)
+
+            print("A configuração MaxCircuitDirtiness foi definida para 10 minutos.")
+    except Exception as e:
+        print(f"Erro: {e}")
+
