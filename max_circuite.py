e36737ad5c412ed1e7f39df79064e0800a74f68c
zopher3 <146519503+zopher3@users.noreply.github.com>
2023-10-23 17:31:28 +0100

    Update max_circuite.py

--- max_circuite.py
+++ max_circuite.py
@@ -1,11 +1,13 @@
 import stem.process
 
-def circuite_max():
+# Caminho para o arquivo de configuração do Tor
 torrc_path = "/etc/tor/torrc"  # Altere o caminho conforme necessário
 
+# Defina a nova configuração MaxCircuitDirtiness (10 segundos)
 new_max_circuit_dirtiness = "MaxCircuitDirtiness 10 seconds"
 
 try:
+    # Inicialize o processo Tor com o arquivo de configuração
     tor_process = stem.process.launch_tor_with_config(
         torrc_path=torrc_path,
     )
